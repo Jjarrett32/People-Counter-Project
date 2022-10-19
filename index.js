3fcdc90b1d41a26f61f4f4f6431ca32dfaa1a659
Jabarre Jarrett <jjarrett.prsvr@gmail.com>
2022-10-18 21:00:44 -0500

    finished project

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let count = 0
+let saveEl = document.getElementById
+("save-el")
 let countEl = document.getElementById("count-el")
 function increment() {
     count = count + 1
     countEl.innerText = count
-    console.log(count)
+    
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent = saveEl.textContent + countStr
+    countEl.textContent = 0
+    count = 0
 }
-save();
 
 
 
-//let countEl = document.getElementById("count-el")
-//countEl.innerText = count
